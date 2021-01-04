106f93e5cbff723f39171888915dc7911c5eded2
Robert Zamora <zamora.rr@gmail.com>
2021-01-03 23:33:27 -0500

    add progress callback to show objective only

--- src/lcpa.cpp
+++ src/lcpa.cpp
@@ -45,49 +45,17 @@ private:
   // loss computer
   LossComputer* computer;
 
+  // keep track of display information
+  std::time_t time_last_update = std::time(0);
+  std::time_t time_current = std::time(0);
+
 public:
   // Constructor with data
   LossCutCallback(const IloIntVarArray &_lambda, IloNumVar &_L, LossComputer* _computer):
   lambda(_lambda), L(_L), computer(_computer) {}
 
-  inline void lazyCut(const IloCplex::Callback::Context &context) {
-    IloEnv env = context.getEnv();
-
-    //env.out() << "candidate lambda: [" ;
-    arma::vec lambda_val(lambda.getSize());
-    for (int j = 0; j < lambda.getSize(); j++) {
-      lambda_val[j] = context.getCandidatePoint(lambda[j]);
-    }
-
-    // calculate actual loss at this lambda
-    // compare that to the candidate objective
-    // if actual loss is still greater than candidate loss,
-    // add a new cut to improve the candidate
-    // us
-    //
-    // Vmin = candidate? incumbent? value of objective function (uses cutting planes for loss function)
-    // Vmax = best value using actual loss function?
-
-    // add lazy constraint
-    double loss_point = computer->loss(lambda_val);
-    arma::vec loss_slope = computer->loss_grad(lambda_val);
-
-    // check that loss_point is ?
-    double L_val = context.getCandidatePoint(L);
-    if (abs(loss_point - L_val) <= 1E-8) {
-      //env.out() << "close enough, not adding a new constraint" << endl;
-      return;
-    }
-
-    //  L >= loss_approx + sum_expr(colwise(loss_grad_approx[j]) * lambda[j], j = 1:d) - q
-    IloNumExpr sum_expr = IloExpr(context.getEnv());
-    for (int j = 0; j < lambda.getSize(); j++) {
-      sum_expr += loss_slope[j]*(lambda[j] - lambda_val[j]);
-    }
-
-    context.rejectCandidate(L - loss_point - sum_expr >= 0.0);
-    sum_expr.end();
-  }
+  // lazycut callback
+  void lazyCut(const IloCplex::Callback::Context &context);
 
   // This is the function that we have to implement and that CPLEX will call
   // during the solution process at the places that we asked for.
@@ -106,7 +74,59 @@ public:
 void LossCutCallback::invoke(const IloCplex::Callback::Context &context) {
   if (context.inCandidate()) {
     lazyCut(context);
+  } else if (context.inGlobalProgress()) {
+    time_current = std::time(0);
+    if (time_current - time_last_update >= 10) {
+      // update every 10 seconds
+      time_last_update = time_current;
+      IloNum V = context.getIncumbentObjective();
+
+      IloEnv env = context.getEnv();
+      env.setOut(Rcpp::Rcout);
+      env.out() << "[" << time_current << "] objective value: " << V << "\n";
+    }
   }
+}
+
+
+// lazycut callback
+void LossCutCallback::lazyCut(const IloCplex::Callback::Context &context) {
+  IloEnv env = context.getEnv();
+
+  //env.out() << "candidate lambda: [" ;
+  arma::vec lambda_val(lambda.getSize());
+  for (int j = 0; j < lambda.getSize(); j++) {
+    lambda_val[j] = context.getCandidatePoint(lambda[j]);
+  }
+
+  // calculate actual loss at this lambda
+  // compare that to the candidate objective
+  // if actual loss is still greater than candidate loss,
+  // add a new cut to improve the candidate
+  // us
+  //
+  // Vmin = candidate? incumbent? value of objective function (uses cutting planes for loss function)
+  // Vmax = best value using actual loss function?
+
+  // add lazy constraint
+  double loss_point = computer->loss(lambda_val);
+  arma::vec loss_slope = computer->loss_grad(lambda_val);
+
+  // check that loss_point is ?
+  double L_val = context.getCandidatePoint(L);
+  if (abs(loss_point - L_val) <= 1E-8) {
+    //env.out() << "close enough, not adding a new constraint" << endl;
+    return;
+  }
+
+  //  L >= loss_approx + sum_expr(colwise(loss_grad_approx[j]) * lambda[j], j = 1:d) - q
+  IloNumExpr sum_expr = IloExpr(env);
+  for (int j = 0; j < lambda.getSize(); j++) {
+    sum_expr += loss_slope[j]*(lambda[j] - lambda_val[j]);
+  }
+
+  context.rejectCandidate(L - loss_point - sum_expr >= 0.0);
+  sum_expr.end();
 }
 
 
@@ -172,6 +192,7 @@ Rcpp::List lcpa_cpp(arma::mat x, arma::vec y, std::string logfile, int R_max = 3
     LossCutCallback cb(lambda, L, &computer);
     CPXLONG contextMask = 0;
     contextMask |= IloCplex::Callback::Context::Id::Candidate;
+    contextMask |= IloCplex::Callback::Context::Id::GlobalProgress;
     cplex.use(&cb, contextMask);
 
     // cplex parameters
